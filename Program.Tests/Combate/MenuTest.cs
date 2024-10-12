a92fb4ab944b1dbfc67435ad62fc738baba1c528
ConstanzaBlanco <constanza.blanco@correo.ucu.edu.uy>
2024-10-12 01:36:31 -0300

    test iniciado

---
+++ Program.Tests/Combate/MenuTest.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using Library.Combate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Program.Tests.Combate;
+
+[TestClass]
+[TestSubject(typeof(Menu))]
+public class MenuTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
